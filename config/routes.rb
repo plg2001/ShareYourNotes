ccc622582ab57b5a608c4eba4ad0e32b992c93d8
Pierluca WSL <pierlucagrasso89@gmail.com>
2023-06-11 17:04:16 +0200

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root  'pages#home'
   get 'about' => 'pages#about'
+
+  
+  post 'invia_messaggio', to: 'admin/dashboard#invia_messaggio'
+
   
   
 
