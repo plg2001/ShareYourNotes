df993399c3c171f0cfa9f734f27953ac2ee0d1ea
Pierluca WSL <pierlucagrasso89@gmail.com>
2023-06-12 11:56:22 +0200

    Change Tags Table

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,32 +2,21 @@
 ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
-  content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  content title: proc { I18n.t("active_admin.dashboard") } do    
+    panel "Utenti registrati" do
+      table_for User.all do
+        column :email
+        column :encrypted_password
+        column :created_at
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+    panel "Tags" do
+      table_for Tag.all do
+        column :body
+        column :created_at
+      end
+    end
+    
+  end 
 end
