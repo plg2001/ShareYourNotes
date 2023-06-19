7fdc744a5d9f4119a2f01a96024d14f836bab457
Michael Corelli <michaelcorelli11@gmail.com>
2023-06-19 12:30:35 +0200

    Fifth Commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,6 +75,10 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
 
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+
+
+
 end
