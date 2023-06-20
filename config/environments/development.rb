8c320013bc8fef7e46db00c1809313400bd99dc5
Pierluca WSL <pierlucagrasso89@gmail.com>
2023-06-20 17:47:00 +0200

    Welcome email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,6 +79,21 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'sharesyournotes@gmail.com'}
 
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'pierlucagrasso89@gmail.com',
+  password:             'ogxointajurpeioe',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
 
 end
