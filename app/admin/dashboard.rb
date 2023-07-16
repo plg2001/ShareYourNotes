c24c7b0b2a805fa77d7f8b593148cf9ba0241dd4
Pierluca WSL <pierlucagrasso89@gmail.com>
2023-07-16 16:23:55 +0200

    Fix active admin

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,6 +2,10 @@
 ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
+   action_item :back_to_home, only: :index do
+    link_to "Torna alla Home", root_path
+  end
+  
   content title: proc { I18n.t("active_admin.dashboard") } do    
     panel "Utenti registrati" do
       table_for User.all do
