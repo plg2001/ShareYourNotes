f0a73c6e61d07847711aaae0001a9ea5c3020e3f
Pierluca WSL <pierlucagrasso89@gmail.com>
2023-08-10 19:55:44 +0200

    Profile box on show_note

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,2 +1,9 @@
 module NotesHelper
+    def user_online_status1(user)
+        if user.last_seen.present? && user.last_seen > 10.minutes.ago &&  user.last_seen != nil
+          content_tag(:span, "", class: "online-indicator1 online")
+        else
+          content_tag(:span,"", class: "online-indicator1 offline")
+        end
+    end
 end
