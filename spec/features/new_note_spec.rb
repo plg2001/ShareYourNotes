429763d357cf9f7fb0b73f3ae64c09321ea5d7ce
Michael Corelli <michaelcorelli11@gmail.com>
2023-08-19 19:23:30 +0200

    fix Rspec

--- spec/features/new_note_spec.rb
+++ spec/features/new_note_spec.rb
@@ -9,23 +9,47 @@ RSpec.feature ' New Note', type: :feature do
         password: 'password'
         )
     }
-    let(:tag) { Tag.create(
-        name: 'Limiti Analisi'
-        )
+
+    let!(:tag1) {
+      Tag.create(name: 'Limiti Analisi')
     }
-    let(:topic) { Topic.create(
-            name: 'Analisi 1'
-        )
+  
+    let!(:tag2) {
+      Tag.create(name: 'Serie di Fourier')
     }
-    let(:faculty) { Faculty.create(
-        name: 'Ingegneria Informatica'
-        )
+  
+    let!(:tag3) {
+      Tag.create(name: 'Regressione Lineare')
+    }
+    
+    let!(:topic1) {
+      Topic.create(name: 'Analisi 1')
+    }
+  
+    let!(:topic2) {
+      Topic.create(name: 'Analisi 2')
+    }
+  
+    let!(:topic3) {
+      Topic.create(name: 'Fondamenti di IA')
+    }
+
+    let!(:faculty1) {
+      Faculty.create(name: 'Ingegneria Informatica')
+    }
+  
+    let!(:faculty2) {
+      Faculty.create(name: 'Ingegneria Meccanica')
+    }
+  
+    let!(:faculty3) {
+      Faculty.create(name: 'Chimica')
     }
 
   scenario 'A valid user uploads a valid note' do
-    tag
-    topic
-    faculty
+    tag1
+    topic1
+    faculty1
     user 
     user.confirm
     visit signin_path
@@ -38,15 +62,15 @@ RSpec.feature ' New Note', type: :feature do
     fill_in 'Description', with: 'note description'
     attach_file('file', Rails.root + 'spec/support/files/test_file.docx')
 
-    expect(find('#tag'+tag.id.to_s)).not_to be_checked
-    find('input', id: 'tag'+tag.id.to_s).click
+    expect(find('#tag'+tag1.id.to_s)).not_to be_checked
+    find('input', id: 'tag'+tag1.id.to_s).click
 
-    expect(find("#tag"+tag.id.to_s)).to be_checked
+    expect(find("#tag"+tag1.id.to_s)).to be_checked
 
-    expect(find('#topic'+topic.id.to_s)).not_to be_checked
-    find('input', id: 'topic'+topic.id.to_s).click
+    expect(find('#topic'+topic1.id.to_s)).not_to be_checked
+    find('input', id: 'topic'+topic1.id.to_s).click
 
-    expect(find("#topic"+topic.id.to_s)).to be_checked
+    expect(find("#topic"+topic1.id.to_s)).to be_checked
     
     expect(page).to have_select('note_faculty_id')
 
@@ -60,9 +84,9 @@ RSpec.feature ' New Note', type: :feature do
   end
 
   scenario 'A valid user uploads an invalid note because miss the name' do
-    tag
-    topic
-    faculty
+    tag1
+    topic1
+    faculty1
     user 
     user.confirm
     visit signin_path
@@ -75,15 +99,15 @@ RSpec.feature ' New Note', type: :feature do
 
     attach_file('file', Rails.root + 'spec/support/files/test_file.docx')
 
-    expect(find('#tag'+tag.id.to_s)).not_to be_checked
-    find('input', id: 'tag'+tag.id.to_s).click
+    expect(find('#tag'+tag1.id.to_s)).not_to be_checked
+    find('input', id: 'tag'+tag1.id.to_s).click
 
-    expect(find("#tag"+tag.id.to_s)).to be_checked
+    expect(find("#tag"+tag1.id.to_s)).to be_checked
 
-    expect(find('#topic'+topic.id.to_s)).not_to be_checked
-    find('input', id: 'topic'+topic.id.to_s).click
+    expect(find('#topic'+topic1.id.to_s)).not_to be_checked
+    find('input', id: 'topic'+topic1.id.to_s).click
 
-    expect(find("#topic"+topic.id.to_s)).to be_checked
+    expect(find("#topic"+topic1.id.to_s)).to be_checked
     
     expect(page).to have_select('note_faculty_id')
 
@@ -97,9 +121,9 @@ RSpec.feature ' New Note', type: :feature do
   end
 
   scenario 'A valid user uploads an invalid note because miss the description' do
-    tag
-    topic
-    faculty
+    tag1
+    topic1
+    faculty1
     user 
     user.confirm
     visit signin_path
@@ -112,15 +136,15 @@ RSpec.feature ' New Note', type: :feature do
 
     attach_file('file', Rails.root + 'spec/support/files/test_file.docx')
 
-    expect(find('#tag'+tag.id.to_s)).not_to be_checked
-    find('input', id: 'tag'+tag.id.to_s).click
+    expect(find('#tag'+tag1.id.to_s)).not_to be_checked
+    find('input', id: 'tag'+tag1.id.to_s).click
 
-    expect(find("#tag"+tag.id.to_s)).to be_checked
+    expect(find("#tag"+tag1.id.to_s)).to be_checked
 
-    expect(find('#topic'+topic.id.to_s)).not_to be_checked
-    find('input', id: 'topic'+topic.id.to_s).click
+    expect(find('#topic'+topic1.id.to_s)).not_to be_checked
+    find('input', id: 'topic'+topic1.id.to_s).click
 
-    expect(find("#topic"+topic.id.to_s)).to be_checked
+    expect(find("#topic"+topic1.id.to_s)).to be_checked
     
     expect(page).to have_select('note_faculty_id')
 
@@ -134,9 +158,9 @@ RSpec.feature ' New Note', type: :feature do
   end
 
   scenario 'A valid user uploads an invalid note because miss the file' do
-    tag
-    topic
-    faculty
+    tag1
+    topic1
+    faculty1
     user 
     user.confirm
     visit signin_path
@@ -148,15 +172,15 @@ RSpec.feature ' New Note', type: :feature do
     fill_in 'Name', with: 'note test'
     fill_in 'Description', with: 'note description'
 
-    expect(find('#tag'+tag.id.to_s)).not_to be_checked
-    find('input', id: 'tag'+tag.id.to_s).click
+    expect(find('#tag'+tag1.id.to_s)).not_to be_checked
+    find('input', id: 'tag'+tag1.id.to_s).click
 
-    expect(find("#tag"+tag.id.to_s)).to be_checked
+    expect(find("#tag"+tag1.id.to_s)).to be_checked
 
-    expect(find('#topic'+topic.id.to_s)).not_to be_checked
-    find('input', id: 'topic'+topic.id.to_s).click
+    expect(find('#topic'+topic1.id.to_s)).not_to be_checked
+    find('input', id: 'topic'+topic1.id.to_s).click
 
-    expect(find("#topic"+topic.id.to_s)).to be_checked
+    expect(find("#topic"+topic1.id.to_s)).to be_checked
     
     expect(page).to have_select('note_faculty_id')
 
@@ -170,9 +194,9 @@ RSpec.feature ' New Note', type: :feature do
   end
 
   scenario 'A valid user uploads an invalid note because miss the tags' do
-    tag
-    topic
-    faculty
+    tag1
+    topic1
+    faculty1
     user 
     user.confirm
     visit signin_path
@@ -186,10 +210,10 @@ RSpec.feature ' New Note', type: :feature do
 
     attach_file('file', Rails.root + 'spec/support/files/test_file.docx')
 
-    expect(find('#topic'+topic.id.to_s)).not_to be_checked
-    find('input', id: 'topic'+topic.id.to_s).click
+    expect(find('#topic'+topic1.id.to_s)).not_to be_checked
+    find('input', id: 'topic'+topic1.id.to_s).click
 
-    expect(find("#topic"+topic.id.to_s)).to be_checked
+    expect(find("#topic"+topic1.id.to_s)).to be_checked
     
     expect(page).to have_select('note_faculty_id')
 
@@ -203,9 +227,9 @@ RSpec.feature ' New Note', type: :feature do
   end
 
   scenario 'A valid user uploads an invalid note because miss the topics' do
-    tag
-    topic
-    faculty
+    tag1
+    topic1
+    faculty1
     user 
     user.confirm
     visit signin_path
@@ -219,10 +243,10 @@ RSpec.feature ' New Note', type: :feature do
 
     attach_file('file', Rails.root + 'spec/support/files/test_file.docx')
 
-    expect(find('#tag'+tag.id.to_s)).not_to be_checked
-    find('input', id: 'tag'+tag.id.to_s).click
+    expect(find('#tag'+tag1.id.to_s)).not_to be_checked
+    find('input', id: 'tag'+tag1.id.to_s).click
 
-    expect(find("#tag"+tag.id.to_s)).to be_checked
+    expect(find("#tag"+tag1.id.to_s)).to be_checked
     
     expect(page).to have_select('note_faculty_id')
 
@@ -236,9 +260,9 @@ RSpec.feature ' New Note', type: :feature do
   end
 
   scenario 'A valid user uploads an invalid note because miss the faculty' do
-    tag
-    topic
-    faculty
+    tag1
+    topic1
+    faculty1
     user 
     user.confirm
     visit signin_path
@@ -252,15 +276,15 @@ RSpec.feature ' New Note', type: :feature do
 
     attach_file('file', Rails.root + 'spec/support/files/test_file.docx')
 
-    expect(find('#tag'+tag.id.to_s)).not_to be_checked
-    find('input', id: 'tag'+tag.id.to_s).click
+    expect(find('#tag'+tag1.id.to_s)).not_to be_checked
+    find('input', id: 'tag'+tag1.id.to_s).click
 
-    expect(find("#tag"+tag.id.to_s)).to be_checked
+    expect(find("#tag"+tag1.id.to_s)).to be_checked
 
-    expect(find('#topic'+topic.id.to_s)).not_to be_checked
-    find('input', id: 'topic'+topic.id.to_s).click
+    expect(find('#topic'+topic1.id.to_s)).not_to be_checked
+    find('input', id: 'topic'+topic1.id.to_s).click
 
-    expect(find("#topic"+topic.id.to_s)).to be_checked
+    expect(find("#topic"+topic1.id.to_s)).to be_checked
 
     click_button 'Carica'
     
