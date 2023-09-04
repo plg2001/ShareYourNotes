29be25b29cbbc1287edeace649a80fb43bd5e4b6
Pierluca WSL <pierlucagrasso89@gmail.com>
2023-09-04 17:20:45 +0200

    Aggiunta 100 utenti di test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+
+i = 81
+
+while i <= 100
+  user = User.create(name: "test"+i.to_s, username: "test"+i.to_s,email: "test"+ i.to_s+ "@gmail.com",password: "test12345",password_confirmation: "test12345",
+  last_seen: "2023-09-01 18:45:52.106063000 +0000")
+  user.skip_confirmation!
+  user.save!
+  i += 1
+end
